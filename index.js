aef4017b6639bb4f15434e931dcc2608b564aca1
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2023-02-27 12:07:56 +0100

    first fixes with animationOne

--- index.js
+++ index.js
@@ -2,64 +2,74 @@
 let body = document.querySelector('body')
 let wrapper = document.querySelector('.wrapper')
 
-function startUp () {
-  body.addEventListener('keyup', keyUp)
+function startUp() {
+    body.addEventListener('keyup', keyUp)
 }
 
-function keyUp (event) {
-  let numberString = event.key
-  let pressedNr = parseInt(numberString)
-  switch (pressedNr) {
-    case 0:
-      animationZero()
-      break
-    case 1:
-      animationOne()
-      break
-    case 2:
-      animationTwo()
-      break
-    case 3:
-      animationThree()
-      break
-    case 4:
-      animationFour()
-      break
-    case 5:
-      animationFive()
-      break
-    case 6:
-      animationSix()
-      break
-    case 7:
-      animationSeven()
-      break
-    case 8:
-      animationEight()
-      break
-    case 9:
-      animationNine()
-      break
-    default:
-      break
-  }
+function keyUp(event) {
+    let numberString = event.key
+    let pressedNr = parseInt(numberString)
+    switch (pressedNr) {
+        case 0:
+            animationZero()
+            break
+        case 1:
+            animationOne()
+            break
+        case 2:
+            animationTwo()
+            break
+        case 3:
+            animationThree()
+            break
+        case 4:
+            animationFour()
+            break
+        case 5:
+            animationFive()
+            break
+        case 6:
+            animationSix()
+            break
+        case 7:
+            animationSeven()
+            break
+        case 8:
+            animationEight()
+            break
+        case 9:
+            animationNine()
+            break
+        default:
+            break
+    }
 }
 
-function animationZero () {
-  let div = document.createElement('div')
-  div.classList.add('animationZero')
-  wrapper.append(div)
+function animationZero() {
+    let div = document.createElement('div')
+    div.classList.add('animationZero')
+    wrapper.append(div)
 
-  setTimeout(() => {
-    div.classList.add('changeZero')
-  }, 200)
+    setTimeout(() => {
+        div.classList.add('changeZero')
+    }, 200)
 
-  setTimeout(() => {
-    div.classList.remove('changeZero')
-  }, 800)
+    setTimeout(() => {
+        div.classList.remove('changeZero')
+    }, 800)
 
-  setTimeout(() => {
-    div.classList.remove('animationZero')
-  }, 1200)
+    setTimeout(() => {
+        div.classList.remove('animationZero')
+    }, 1200)
+}
+
+function animationOne() {
+    let div = document.createElement('div')
+    div.classList.add('animationOne')
+    wrapper.append(div)
+
+    setTimeout(() => {
+        div.classList.remove('animationOne')
+    }, 2000)
 }
 startUp()
